73d71861ab3f25842ece8c0e58bdf1a39024935d
Thomas Madden <tmadden@gmail.com>
2023-05-24 09:48:54 -0400

    Add a reasonable attempt

--- ports/cradle/portfile.cmake
+++ ports/cradle/portfile.cmake
@@ -1,16 +1,21 @@
+set(CRADLE_COMMIT_HASH "211d7bf09b1cafe1d620f9a9f7cce7576f1920b1")
+
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO open-cradle/cradle
-  # TODO: REF 71b11aeabdb92b5269270c3c8953a58c6e108818
-  # TODO: SHA512 0
-  HEAD_REF main
-)
+  REF ${CRADLE_COMMIT_HASH}
+  SHA512 e8aac5944ad3c1e23db79b8b16ff50522f777e057d4a57225f4b07f5560ee35983b2f22b6c1308acb7bad67b4a8cb111a4622271975898c35cb071c784db0ff5)
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
-  PREFER_NINJA
-)
-vcpkg_install_cmake()
+  OPTIONS
+    -DCRADLE_USE_CONAN=OFF
+    -DCRADLE_SUPPORT_VCPKG=ON
+    -DCRADLE_IS_MAIN_PROJECT=OFF
+    -DCRADLE_PREPROCESSOR_USAGE=GitCache
+    -DCRADLE_COMMIT_HASH=${CRADLE_COMMIT_HASH})
+
+vcpkg_cmake_install()
 vcpkg_fixup_cmake_targets()
 
 file(
